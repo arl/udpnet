346f61214a954c2a4c09d74b39f297b668f7f984
Aurélien Rainone <aurelien.rainone@gmail.com>
2017-02-23 10:36:12 +0100

    Add TestConnectionJoin

---
+++ conn_test.go
@@ -0,0 +1,87 @@
+package udp
+
+import (
+	"fmt"
+	"net"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+type dummyCallback struct{}
+
+func (dc dummyCallback) OnStart() {
+	fmt.Println("start")
+}
+func (dc dummyCallback) OnStop() {
+	fmt.Println("stop")
+}
+func (dc dummyCallback) OnConnect() {
+	fmt.Println("connect")
+}
+func (dc dummyCallback) OnDisconnect() {
+	fmt.Println("disconnect")
+}
+
+func TestConnectionJoin(t *testing.T) {
+	const (
+		ServerPort = 30000
+		ClientPort = 30001
+		ProtocolId = 0x11112222
+		DeltaTime  = 0.001
+		SendRate   = 0.25
+		TimeOut    = 1.5
+	)
+
+	client := NewConn(dummyCallback{}, ProtocolId, TimeOut)
+	server := NewConn(dummyCallback{}, ProtocolId, TimeOut)
+
+	require.True(t, client.Start(ClientPort), "couldn't start client connection")
+	defer client.Stop()
+
+	require.True(t, server.Start(ServerPort), "couldn't start server connection")
+	defer server.Stop()
+
+	cAddr, _ := net.ResolveUDPAddr("udp", fmt.Sprintf("127.0.0.1:%d", ServerPort))
+	client.Connect(cAddr)
+	server.Listen()
+
+	for {
+		if client.IsConnected() && server.IsConnected() {
+			break
+		}
+
+		if !client.IsConnecting() && client.ConnectFailed() {
+			break
+		}
+
+		clientPacket := []byte("client to server")
+		client.SendPacket(clientPacket)
+
+		serverPacket := []byte("server to client")
+		server.SendPacket(serverPacket)
+
+		for {
+			var packet [256]byte
+			bytesRead := client.ReceivePacket(packet[:])
+			if bytesRead == 0 {
+				break
+			}
+		}
+
+		for {
+			var packet [256]byte
+			bytesRead := server.ReceivePacket(packet[:])
+			if bytesRead == 0 {
+				break
+			}
+		}
+
+		client.Update(DeltaTime)
+		server.Update(DeltaTime)
+	}
+
+	assert.True(t, client.IsConnected(), "client should be connected")
+	assert.True(t, server.IsConnected(), "server should be connected")
+}
